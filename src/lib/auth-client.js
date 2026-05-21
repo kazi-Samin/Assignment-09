58b6fc7c08a7b1bc1dd66089a779d34615423bbd
kazisamin0173-byte <kazisamin0173@gmail.com>
2026-05-22 03:59:54 +0600

    some change to figure the problem

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,97 +1,21 @@
-// "use client";
-
-// import { createAuthClient } from "better-auth/react";
-
-// export const authClient =
-//   createAuthClient({
-//     baseURL:
-//       process.env.NEXT_PUBLIC_API_URL,
-
-//     fetchOptions: {
-//       credentials: "include",
-//     },
-//   });
-
-// export const {
-//   signIn,
-//   signUp,
-//   signOut,
-//   useSession,
-// } = authClient;
-
 "use client";
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL;
+import { createAuthClient } from "better-auth/react";
 
-export async function loginUser(
-  email,
-  password
-) {
-  const response = await fetch(
-    `${API_URL}/login`,
-    {
-      method: "POST",
+export const authClient =
+  createAuthClient({
+    baseURL:
+      process.env.NEXT_PUBLIC_API_URL,
 
-      headers: {
-        "Content-Type":
-          "application/json",
-      },
-
+    fetchOptions: {
       credentials: "include",
+    },
+  });
 
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    }
-  );
+export const {
+  signIn,
+  signUp,
+  signOut,
+  useSession,
+} = authClient;
 
-  return response.json();
-}
-
-export async function registerUser(
-  data
-) {
-  const response = await fetch(
-    `${API_URL}/register`,
-    {
-      method: "POST",
-
-      headers: {
-        "Content-Type":
-          "application/json",
-      },
-
-      credentials: "include",
-
-      body: JSON.stringify(data),
-    }
-  );
-
-  return response.json();
-}
-
-export async function getUser() {
-  const response = await fetch(
-    `${API_URL}/me`,
-    {
-      credentials: "include",
-    }
-  );
-
-  return response.json();
-}
-
-export async function logoutUser() {
-  const response = await fetch(
-    `${API_URL}/logout`,
-    {
-      method: "POST",
-
-      credentials: "include",
-    }
-  );
-
-  return response.json();
-}
