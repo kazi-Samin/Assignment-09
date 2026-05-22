dce5fc7db8e6d90682dd579093e0f132895fc8cd
kazisamin0173-byte <kazisamin0173@gmail.com>
2026-05-22 16:44:16 +0600

    some change

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-// import { authClient } from "@/lib/auth-client";
+import { authClient } from "@/lib/auth-client";
 import {
   registerUser,
   getCurrentUser,
